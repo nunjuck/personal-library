8b393a904d6440243f5ce92795dd4d8d292404a5
Nikita Koshelev <koshelevn92@gmail.com>
2021-09-03 14:01:53 +0300

    feat: add logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import Header from './components/Header'
+
 const App = () => {
-  return <h1>Library books</h1>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <Logo />
+      </header>
+    </div>
+  )
+}
+
+export default Header
