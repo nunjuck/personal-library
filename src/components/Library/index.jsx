5d10adbca5d3a521318fcdcbfc15dd601209d4ce
Nikita Koshelev <koshelevn92@gmail.com>
2021-09-10 16:01:44 +0300

    feat: add page info

--- src/components/Library/index.jsx
+++ src/components/Library/index.jsx
@@ -9,7 +9,21 @@ const Library = () => {
 
   return (
     <div className="container">
-      <h1 className="title-page">Моя библиотека книг</h1>
+      <div className="page-info">
+        <h1 className="page-info__title">Моя библиотека книг</h1>
+        <p className="page-info__subtitle">
+          Выбери книгу и напиши мне в
+          <a
+            className="link"
+            href="https://t.me/nunjuck"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Телеграм
+          </a>
+          .
+        </p>
+      </div>
       <Categories />
       <Books name={name} />
     </div>
