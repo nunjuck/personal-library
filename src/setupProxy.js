b84a3705bced9a3ca40076a4b4751d4888e4a5e9
Nikita Koshelev <koshelevn92@gmail.com>
2021-09-10 10:30:32 +0300

    chore: remove context api proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/notion",
+    "/v1",
     createProxyMiddleware({
       target: "https://api.notion.com/",
-      pathRewrite: { "^/notion": "" },
+      // pathRewrite: { "^/notion": "" },
       changeOrigin: true,
     })
   );
