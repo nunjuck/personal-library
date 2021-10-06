34311c344421cb284251626c5a2f72c7224df662
Nikita Koshelev <koshelevn92@gmail.com>
2021-10-06 18:02:05 +0300

    test: add Book tests

--- src/components/Book/index.test.tsx
+++ src/components/Book/index.test.tsx
@@ -9,8 +9,8 @@ const testBook = {
   id: "4546542124687845",
 };
 
-describe("Book", () => {
-  render(
+const setup = () => {
+  const book = render(
     <Book
       cover={testBook.cover}
       title={testBook.title}
@@ -19,9 +19,43 @@ describe("Book", () => {
     />
   );
 
-  it("render button for copy name book", () => {
-    expect(screen.getByRole("button")).toBeInTheDocument();
-    expect(screen.getByRole("button")).toBeEnabled();
+  const button = book.getByRole("button");
+  const name = book.getByRole("heading");
+  const cover = book.getByRole("img");
+
+  return {
+    button,
+    name,
+    cover,
+    ...book,
+  };
+};
+
+describe("Book", () => {
+  test("render root element", () => {
+    const book = setup();
+    expect(book.getByRole("article")).toBeInTheDocument();
+  });
+
+  test("Book renders name", () => {
+    const { name } = setup();
+    expect(name).toBeInTheDocument();
+  });
+
+  test("Book renders cover", () => {
+    const { cover } = setup();
+    expect(cover).toBeInTheDocument();
+  });
+
+  test("Book renders with data", () => {
+    const book = setup();
+    expect(book).toMatchSnapshot();
+  });
+
+  test("Book renders button for copy name book", () => {
+    const { button } = setup();
+    expect(button).toBeInTheDocument();
+    expect(button).toBeEnabled();
     expect(screen.getByText(/Скопировать/i)).toBeInTheDocument();
   });
 });
